f622c46b7e1f90f259cb98ef4034839bc2cce3c9
Arthur-Cardoso0 <ac346284@gmail.com>
2026-08-27 14:34:31 -0300

    adiciona registro de usuários e aprimora a interface de login e gerenciamento de tarefas

--- Controllers/ContaController.cs
+++ Controllers/ContaController.cs
@@ -21,6 +21,40 @@ namespace TaskTracker.Controllers
     }
 
     [HttpGet]
+    public IActionResult Registrar()
+        {
+            return View();
+        }
+
+    [HttpPost]
+    public async Task<IActionResult> Registrar(string username, string password, string confirmPassword)
+        {
+            if(string.IsNullOrEmpty(username) | string.IsNullOrEmpty(password))
+            {
+                ModelState.AddModelError(string.Empty, "Preencha todos os campos");
+                return View();
+            }
+            //verifica se existe
+            var usuarioExiste = _context.Usuarios.Any(u => u.Nome == username);
+            if (usuarioExiste)
+            {
+                ModelState.AddModelError(string.Empty, "Este nome de usuario já esta em uso");
+                return View();
+            }
+            if(password != confirmPassword)
+            {
+                ModelState.AddModelError(string.Empty, "As senhas não coicidem.");
+                return View();
+            }
+            var novoUsuario = new Usuario{Nome=username};
+            novoUsuario.SenhaHash = _passwordHasher.HashPassword(novoUsuario, password);
+            //salva no banco de dados
+            _context.Usuarios.Add(novoUsuario);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Login");
+        }
+    [HttpGet]
     public IActionResult Index() => View();
 
     [HttpGet]
@@ -29,7 +63,7 @@ namespace TaskTracker.Controllers
     [HttpPost]
     public async Task<IActionResult> Login(string Username, string Password)
     {
-        var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == Username);
+        var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Nome == Username);
         if (usuario == null)
         {
             ModelState.AddModelError(string.Empty, "Usuário ou senha inválidos.");
@@ -51,6 +85,7 @@ namespace TaskTracker.Controllers
         };
         //cria e salva o cookie no navegador
         var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
         return RedirectToAction("Index", "Home");
     }
 
